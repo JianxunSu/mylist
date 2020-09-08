a5449978f30b72794f67e5a97a3f965b3194ae01
Jevin Su <jevin.su@yahoo.com>
2020-09-07 20:32:13 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App0() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +20,26 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(10);
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `Yes, You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You just clicked {count} times</p>
+      {/*Something*/}
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
